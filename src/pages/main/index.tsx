8a41218cc146d6da654c1bbe668a50b57e1da119
Daniel Usov <danielusovdev@gmail.com>
2024-02-21 21:23:18 +0300

    feat: start work

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,10 +1,9 @@
-import { useEffect } from 'react';
 import { PlacesList } from '../../components/places-list';
 import { SearchField } from '../../components/search-field';
 import { useGetPlacesQuery } from '../../data/services/place';
 
 export const MainPage = () => {
-  const { isError, data } = useGetPlacesQuery();
+  const { data } = useGetPlacesQuery();
 
   return (
     <>
