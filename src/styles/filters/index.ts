c78b326412fa77e788e12cf3eadc90b81ef8dd05
Daniel Usov <dan.usov82@gmail.com>
2024-03-28 17:51:05 +0300

    feat: base select

--- src/styles/filters/index.ts
+++ src/styles/filters/index.ts
@@ -1,20 +1,10 @@
 import styled from 'styled-components';
 
-export const FiltersSelectS = styled.select`
+export const FiltersSelectS = styled.div`
   padding: 1vh 4vw 1vh 4vw;
   margin: 0 1vw 0 0;
   border: 1px solid #d9d9d9;
   border-radius: 4px;
   background-color: #fff;
-
-  &:focus {
-    outline: none;
-  }
-
-  option {
-    width: 100%;
-    padding: 1vh 4vw 1vh 4vw;
-    border: 1px solid #d9d9d9;
-    border-radius: 4px;
-  }
+  width: 20%;
 `;
